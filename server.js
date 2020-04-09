a48bac1dc986ea19256261a704db05d2ebd4e79a
Sanjay Kumar <sanjaykumar2338@yahoo.com>
2020-04-09 12:18:47 +0530

    flash message

--- server.js
+++ server.js
@@ -2,29 +2,47 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-
-
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
 
 const express = require('express')
-const app = express()
 const fs = require('fs')
 const stripe = require('stripe')(stripeSecretKey)
 var bodyParser = require('body-parser');
 let mysql = require('mysql');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var flash = require('connect-flash');
 
-let connection = mysql.createConnection({
-  host: process.env.HOSTNAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
+const app = express();
 
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(flash());
+app.use(cookieParser());
+app.use(session({secret: 'secret123',saveUninitialized:true,resave:true}));
+
+//- MYSQL Module
+try{
+    var mysql_npm = require('./node_modules/mysql');
+}catch(err){
+    console.log("Cannot find `mysql` module. Is it installed ? Try `npm install mysql` or `npm install`.");
+}
+
+//- Connection configuration
+var db_config = {
+    host         : 'localhost',
+    user         : 'root',
+    password     : '',
+    database     : 'dugong'
+};
+
+//- Create the connection variable
+var connection = mysql_npm.createConnection(db_config)
+
+
 
 app.get('/store', function(req, res) {
   fs.readFile('items.json', function(error, data) {
@@ -106,15 +124,60 @@ app.post('/checkout', function(req, res) {
 
 //Manage All the routes of Admin
 var year = new Date().getFullYear();
+
+//For Products
 app.get("/admin", async  (req, res) => {
   res.render('admin/main.ejs',{year:year});
 });
 
 app.get("/admin/product", async  (req, res) => {
-  res.render('admin/products.ejs',{year:year});
+  res.render('admin/product/products.ejs',{year:year});
 });
 
+app.get("/admin/padd", async  (req, res) => {
+  res.render('admin/product/new_product.ejs',{year:year});
+});
 
+//For Category
+app.get("/admin/category", async  (req, res) => {
+  res.locals.message = req.flash();
+	
+  connection.query('Select * from category', function(err,result) {
+	  res.render('admin/category/index.ejs',{year:year,result:result});
+  });	  
 
+});
+
+app.get("/admin/cadd", async  (req, res) => {
+  res.render('admin/category/add.ejs',{year:year});
+});
+
+app.post("/admin/savecategory", async  (req, res) => {
+	var name = req.body.name;
+	
+	var values = [];
+	values.push([name]);
+	
+	connection.query('INSERT INTO category (name) VALUES ?', [values], function(err,result) {
+		if(err) {
+		  console.log('insert error',err);
+		}
+		else {
+		  console.log('inserted success');
+		}
+	});
+	
+	res.redirect('/admin/category');
+});
+
+app.get("/admin/delete_category", async  (req, res) => {
+	let id = req.query.id;
+	
+	var sql = "DELETE FROM category WHERE id = '"+id+"'";
+    connection.query(sql, function(err,result) {
+		req.flash('success', 'Category Deleted successfully');
+		res.redirect('/admin/category');
+	});
+});
 
 app.listen(4000)
